c652d9eaf16e66860a93e53d20665b2ee507f90f
Deepak <deepakdpk1810@gmial.com>
2025-05-24 19:15:35 +0530

    PDF name & config file changes

--- js/config.js
+++ js/config.js
@@ -1,3 +1,1 @@
-const API_BASE_URL = window.location.origin.includes("aahakalyanam.co.in")
-    ? ""  // In production, use relative path
-    : "http://159.89.174.77:5000"; // In dev, talk to local backend
+    const API_BASE_URL = "http://159.89.174.77:5000";
