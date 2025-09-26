d2c9e0971b0c7e04662d04f4b980e32d0c86112f
prasanthoyasco <prasanthoyasco@gmai.com>
2025-09-26 13:48:00 +0530

    live razor pay added

--- images/DEMO2/fitness-app-frontend/src/context/useUserMetrics.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { UserMetricsContext } from './UserMetricsContext';
-
-export const useUserMetrics = () => useContext(UserMetricsContext);
--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-    const API_BASE_URL = "http://159.89.174.77:5000";
+const API_BASE_URL = "https://aahakalyanam.co.in";
